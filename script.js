12e5cc3b44d4179d6203321028aadda5a5c57082
Michael Spari <michaelspari72@gmail.com>
2024-11-11 12:28:15 +0100

    Projekt Struktur

---
+++ script.js
@@ -0,0 +1,20 @@
+function renderDishes() {
+  const dishesFrame = document.getElementById('dishesFrame');
+  dishesFrame.innerHTML = "";
+  for (let index = 0; index < dishes.length; index++) {
+    const dish = dishes[index];
+    dishesFrame.innerHTML += dishesTemplate(dish);
+  }
+}
+
+function dishesTemplate(dish) {
+return `
+    <div class="contentField">
+        <div class="dishesField">
+        <h2>${dish.name}</h2>
+        <p>${dish.description}</p>
+        <p>${dish.price.toFixed(2).replace('.', ',')} €</p>
+        <div>${dish.amount}</div>
+        </div>
+    </div>`;
+}
---
+++ scripts/db.js
@@ -0,0 +1,62 @@
+let dishes =[
+    {
+        "name": "Pizza Margherita", 
+        "price": 15.00,
+        "description": "mit Tomaten, Mozzarella, Basilikum",
+        "amount": 0,
+    },
+    {
+        "name": "Pizza Salami", 
+        "price": 16.00,
+        "description": "mit Tomaten, Mozzarella, Salami",
+        "amount": 0,
+    },
+    {
+        "name": "Pizza Funghi", 
+        "price": 16.00,
+        "description": "mit Tomaten, Mozzarella, Champignons",
+        "amount": 0,
+    },
+    {
+        "name": "Pizza Prosciutto", 
+        "price": 16.00,
+        "description": "mit Tomaten, Mozzarella, Schinken",
+        "amount": 0,
+    },
+    {
+        "name": "Pizza Quattro Stagioni", 
+        "price": 17.00,
+        "description": "mit Tomaten, Mozzarella, Schinken, Champignons, Artischocken, Oliven",
+        "amount": 0,
+    },
+    {
+        "name": "Burger Cheese",
+        "price": 12.00,
+        "description": "mit Rindfleisch, Käse, Tomaten, Gurken, Salat",
+        "amount": 0,
+    },
+    {   "name": "Burger Bacon",
+        "price": 13.00,
+        "description": "mit Rindfleisch, Käse, Tomaten, Gurken, Salat, Bacon",
+        "amount": 0,
+    },
+    {
+        "name": "Burger Veggie",
+        "price": 11.00,
+        "description": "mit Gemüse, Käse, Tomaten, Gurken, Salat",
+        "amount": 0,
+    },
+    {
+        "name": "Burger Chicken",
+        "price": 12.00,
+        "description": "mit Hähnchen, Käse, Tomaten, Gurken, Salat",
+        "amount": 0,
+    },
+    {
+        "name": "Burger Fish",
+        "price": 13.00,
+        "description": "mit Fisch, Käse, Tomaten, Gurken, Salat",
+        "amount": 0,
+    }
+   
+]
