d11911b81c79e0591388ce8a11637f4398e29bf9
Michael Spari <michaelspari72@gmail.com>
2024-11-15 15:45:11 +0100

    Projekt final

--- scripts/template.js
+++ scripts/template.js
@@ -5,11 +5,11 @@ function dishesTemplate(dish) {
         <div class="imageField"><img src="${dish.image}" alt="${dish.name}" /></div>
           <div class="descriptionField"><h2>${dish.name}</h2><p>${dish.description}</p></div>
             <p>${dish.price.toFixed(2).replace('.', ',')} €</p>
-              <div class="buttonField">
-            <img onclick="addDish(${dish.id})" class="addIcon" src="./assets/icons/button_add.jpg">
-          </div>
+            <div class="buttonField">
+          <img onclick="addDish(${dish.id})" class="addIcon" src="./assets/icons/button_add.jpg">
         </div>
-      </div>`;
+      </div>
+    </div>`;
   }
 
 function basketRowsTemplate(dish) {
@@ -22,6 +22,6 @@ function basketRowsTemplate(dish) {
               <img onclick="removeDish(${dish.id})" class="removeIcon" src="./assets/icons/button_remove.jpg">
                 <img onclick="addDish(${dish.id})" class="addIcon" src="./assets/icons/button_add.jpg">
               <img onclick="clearBasket(${dish.id})" class="clearIcon" src="./assets/icons/trash.jpg">
-          </div>
-        </div>`;
+            </div>
+    </div>`;
 }
